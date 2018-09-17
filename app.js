91a01c721af55c9bebfc9cef16daf305dc73303c
ZCW\Administrator <1304485196@qq.com>
2018-09-17 17:07:19 +0800

    koa项目 init

---
+++ app.js
@@ -0,0 +1,34 @@
+// Created by szatpig at 2018/9/13.
+const Koa = require('koa');
+
+const BodyParse = require('koa-bodyparser');
+const logger = require('koa-logger');
+
+const config = require('./config');
+const route = require('./routes');
+
+const app = new Koa();
+
+app.use(BodyParse());
+app.use(logger());
+
+app.use(async (ctx, next) => {
+    try {
+        await next()
+    } catch (err) {
+        ctx.status = err.status || 500;
+        ctx.body = {
+            error: err.message
+        };
+        logger.error(`${err.status} -- ${err.message}\n${err.stack}`)
+    }
+});
+
+
+app.use(route.routes()).use(route.allowedMethods());
+
+app.listen(config.port);
+
+console.log('The first koa project is running : http://localhost:' + config.port);
+
+module.exports = app;
